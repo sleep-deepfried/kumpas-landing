3b19fea8c436426760a78b3857714b6a0c9600cd
sleep-deepfried <pulidoearljohn14@gmail.com>
2024-02-01 01:54:31 +0800

    changed the font color

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -64,7 +64,7 @@ const Features = () =>{
 
             </div>
 
-            <div className="flex flex-col items-center font-bold my-10">
+            <div className="flex flex-col items-center font-bold my-10 text-secondary">
                 <p className="text-8xl">
                     BREAKING THE SILENCE.
                 </p>
